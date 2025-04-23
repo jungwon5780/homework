487624ba25344078183410b5851fb9cb0f6b6679
jungwon5780 <jungwon5780@gmail.com>
2025-04-23 17:01:36 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,63 @@ import profileImg from './assets/images/zi-a.png';
 function App() {
   return (
     <div className="main-container">
-      <h1 className="title">정지아의 리액트 ✨</h1>
+      <h1 className="title">정원의 리액트 🐳</h1>
 
       <ProfileCard
         image={profileImg}
-        name="정지아"
-        description="신소재를 사랑하는 개발자입니다."
+        name="배정원"
+        description="GSITM 부트캠프 진주 10기 "
       />
 
       <div className="link-area">
         <a
           className="link-button"
-          href="https://chapter3-react1.vercel.app/"
+          href="https://onop.vercel.app/hw/0422.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          📘 CHAPTER 3 - 기초
+          📙 0422 과제
         </a>
         <a
           className="link-button"
-          href="https://chapter3-react2.vercel.app/"
+          href="https://onop.vercel.app/hw/0423_1.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          📙 CHAPTER 3 게시판 CRUD
+          🍎 0423 중간과제
         </a>
         <a
           className="link-button"
-          href="https://chapter3-react3.vercel.app/"
+          href="https://onop.vercel.app/hw/0423_2.html"
           target="_blank"
           rel="noopener noreferrer"
         >
-          📗 CHAPTER 3 게시판 + MySQL
+          🧸 0423 과제 1
         </a>
+        <a
+          className="link-button"
+          href="https://onop.vercel.app/hw/0423_3.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          🛫 0423 과제 2
+          </a>
+        <a
+          className="link-button"
+          href="https://onop.vercel.app/hw/0423_4.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          💫 0423 과제 3
+          </a>
+        <a
+          className="link-button"
+          href="https://onop.vercel.app/hw/0423_5.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          🎤 0423 과제 4
+          </a>
       </div>
     </div>
   );
